cab5a224c116ad1917cfed57810719975db536d6
Vu <vu@advanceddatamachines.com>
2020-09-24 11:21:00 -0600

    added vuejs project

---
+++ TicTacToe-VueJS/src/services/apiServices.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default class ApiService {
+	constructor() {
+		this.http = axios.create();
+		this.baseUrl = 'https://localhost:5001/api/game';
+	}
+
+	gameApi() {
+		return {
+			test: () => {
+				return this.http.get(`${this.baseUrl}/test`);
+			},
+			lookForOpponent: (username) => {
+				return this.http.get(`${this.baseUrl}/start/${username}`);
+			},
+			updateMove: (username, gameId, board) => {
+				return this.http.put(`${this.baseUrl}/move/${username}/${gameId}`, board);
+			},
+			quiteGame: (username, gameId) => {
+				return this.http.post(`${this.baseUrl}/quit/${username}/${gameId}`);
+			}
+		};
+	}
+}
